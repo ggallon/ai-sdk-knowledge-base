c43ecb2c2481fedfd323029d099bbcd1f689772c
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-09-24 21:03:54 +0200

    Add and run prettier

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-white dark:bg-zinc-900">
-      <div className="w-full max-w-md overflow-hidden rounded-2xl gap-12 flex flex-col">
+      <div className="flex w-full max-w-md flex-col gap-12 overflow-hidden rounded-2xl">
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
           <h3 className="text-xl font-semibold dark:text-zinc-50">Sign Up</h3>
           <p className="text-sm text-gray-500 dark:text-zinc-400">
@@ -39,7 +39,7 @@ export default function Page() {
         </div>
         <Form action={formAction}>
           <SubmitButton>Sign Up</SubmitButton>
-          <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
+          <p className="mt-4 text-center text-sm text-gray-600 dark:text-zinc-400">
             {"Already have an account? "}
             <Link
               href="/login"
