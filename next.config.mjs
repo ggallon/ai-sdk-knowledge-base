c63c8e57308e65b9a62805254df8333051607f27
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-09-28 12:10:03 +0200

    Update eslint config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // By default, Next.js will run ESLint for all files in the pages/, app/, components/, lib/, and src/ directories
+    dirs: ["ai", "app", "components", "drizzle", "utils"],
+  },
   experimental: {},
   serverExternalPackages: ["pdf-parse"],
 };
