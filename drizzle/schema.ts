dd2da033d127afb64c494d3b9c420f48c7f9c992
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-10-07 20:44:47 +0200

    Use new columns and callback in drizzle table

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,33 +1,27 @@
 import type { Message } from "ai";
-import {
-  pgTable,
-  varchar,
-  text,
-  real,
-  timestamp,
-  json,
-} from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
 
-export const UserTable = pgTable("User", {
-  email: varchar("email", { length: 64 }).primaryKey().notNull(),
-  password: varchar("password", { length: 64 }),
-});
+export const UserTable = pgTable("User", (t) => ({
+  email: t.varchar({ length: 64 }).primaryKey().notNull(),
+  password: t.varchar({ length: 64 }),
+}));
 
-export const ChatTable = pgTable("Chat", {
-  id: text("id").primaryKey().notNull(),
-  createdAt: timestamp("createdAt").notNull(),
-  messages: json("messages").notNull(),
-  author: varchar("author", { length: 64 })
+export const ChatTable = pgTable("Chat", (t) => ({
+  id: t.text().primaryKey().notNull(),
+  createdAt: t.timestamp().notNull(),
+  messages: t.json().notNull(),
+  author: t
+    .varchar({ length: 64 })
     .notNull()
     .references(() => UserTable.email),
-});
+}));
 
-export const ChunkTable = pgTable("Chunk", {
-  id: text("id").primaryKey().notNull(),
-  filePath: text("filePath").notNull(),
-  content: text("content").notNull(),
-  embedding: real("embedding").array().notNull(),
-});
+export const ChunkTable = pgTable("Chunk", (t) => ({
+  id: t.text().primaryKey().notNull(),
+  filePath: t.text().notNull(),
+  content: t.text().notNull(),
+  embedding: t.real().array().notNull(),
+}));
 
 export type Chat = Omit<typeof ChatTable.$inferSelect, "messages"> & {
   messages: Message[];
