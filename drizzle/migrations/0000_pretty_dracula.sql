a2702d7cf56a7a9aee0821553759f3e5f726635d
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-10-13 00:33:48 +0200

    Regenerate drizzle migration files

--- drizzle/migrations/0000_pretty_dracula.sql
+++ drizzle/migrations/0000_pretty_dracula.sql
@@ -1,24 +1,34 @@
 CREATE TABLE IF NOT EXISTS "Chat" (
-	"id" text PRIMARY KEY NOT NULL,
-	"createdAt" timestamp NOT NULL,
+	"id" uuid PRIMARY KEY NOT NULL,
+	"publicId" varchar(32) NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"author" varchar(64) NOT NULL,
 	"messages" json NOT NULL,
-	"author" varchar(64) NOT NULL
+	CONSTRAINT "Chat_publicId_unique" UNIQUE("publicId")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "Chunk" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"chunkRef" text NOT NULL,
 	"filePath" text NOT NULL,
 	"content" text NOT NULL,
-	"embedding" real[] NOT NULL
+	"embedding" real[] NOT NULL,
+	"embeddingVector" vector(1536)
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "User" (
-	"email" varchar(64) PRIMARY KEY NOT NULL,
-	"password" varchar(64)
+	"id" uuid PRIMARY KEY NOT NULL,
+	"email" varchar(64) NOT NULL,
+	"password" varchar(64) NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "User_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "Chat" ADD CONSTRAINT "Chat_author_User_email_fk" FOREIGN KEY ("author") REFERENCES "public"."User"("email") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "Chat" ADD CONSTRAINT "Chat_author_User_email_fk" FOREIGN KEY ("author") REFERENCES "public"."User"("email") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "embeddingVectorIndex" ON "Chunk" USING hnsw ("embeddingVector" vector_cosine_ops);
