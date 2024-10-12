43d111e3a1d8fe95e04713a34225108027e7e8b9
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-10-13 00:21:31 +0200

    Use a custom database connection

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,8 +1,12 @@
-import { sql } from "@vercel/postgres";
+import { createPool } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, {
+const customVercelPool = createPool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(customVercelPool, {
   schema,
   logger: process.env.NODE_ENV === "development",
 });
