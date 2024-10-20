1bdb2899bbf134ea04b9e1d2344e85cf68af8ba1
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-10-21 01:03:18 +0200

    Refactor Files Api routes

--- app/(chat)/api/files/list/route.ts
+++ app/(chat)/api/files/list/route.ts
@@ -1,25 +1,33 @@
 import { list } from "@vercel/blob";
 import { auth } from "@/app/(auth)/auth";
+import { AuthError } from "@/utils/functions";
 
 export async function GET() {
-  const session = await auth();
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      throw new AuthError("Unauthorized");
+    }
 
-  if (!session) {
-    return Response.redirect("/login");
+    const { user } = session;
+    const { blobs } = await list({ prefix: user.email! });
+
+    return Response.json(
+      blobs.map((blob) => ({
+        pathname: blob.pathname.replace(`${user.email!}/`, ""),
+        url: blob.url,
+      })),
+    );
+  } catch (error) {
+    console.error(
+      "API Error handling files list:",
+      error instanceof Error ? error.message : String(error),
+    );
+
+    if (error instanceof AuthError) {
+      return new Response(null, { status: 401 });
+    }
+
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  const { user } = session;
-
-  if (!user) {
-    return Response.redirect("/login");
-  }
-
-  const { blobs } = await list({ prefix: user.email! });
-
-  return Response.json(
-    blobs.map((blob) => ({
-      pathname: blob.pathname.replace(`${user.email}/`, ""),
-      url: blob.url,
-    })),
-  );
 }
--- app/(chat)/api/files/upload/route.ts
+++ app/(chat)/api/files/upload/route.ts
@@ -4,56 +4,67 @@ import { put } from "@vercel/blob";
 import { registry } from "@/ai/setup-registry";
 import { auth } from "@/app/(auth)/auth";
 import { insertChunks } from "@/drizzle/query/chunk";
+import { AuthError, ASKError } from "@/utils/functions";
 import { getPdfContentFromUrl } from "@/utils/pdf";
 
 export async function POST(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const filename = searchParams.get("filename");
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      throw new AuthError("Unauthorized");
+    }
 
-  const session = await auth();
+    if (request.body === null) {
+      throw new ASKError("Request body is empty");
+    }
 
-  if (!session) {
-    return Response.redirect("/login");
+    const { searchParams } = new URL(request.url);
+    const filename = searchParams.get("filename");
+    const { user } = session;
+    const { downloadUrl, pathname, url } = await put(
+      `${user.email}/${filename}`,
+      request.body,
+      {
+        access: "public",
+      },
+    );
+    const content = await getPdfContentFromUrl(downloadUrl);
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+    });
+    const chunkedContent = await textSplitter.createDocuments([content]);
+    const { embeddings } = await embedMany({
+      model: registry.textEmbeddingModel("openai:text-embedding-3-small"),
+      values: chunkedContent.map((chunk) => chunk.pageContent),
+    });
+
+    await insertChunks({
+      chunks: chunkedContent.map((chunk, i) => ({
+        chunkRef: `${user.email}/${filename}/${i}`,
+        filePath: `${user.email}/${filename}`,
+        content: chunk.pageContent,
+        embedding: embeddings[i],
+        embeddingVector: embeddings[i],
+      })),
+    });
+
+    return Response.json({ pathname, url });
+  } catch (error) {
+    console.error(
+      "API Error handling files upload:",
+      error instanceof Error ? error.message : String(error),
+    );
+
+    if (error instanceof AuthError) {
+      return new Response(null, {
+        status: error.message === "Forbidden" ? 403 : 401,
+      });
+    }
+
+    if (error instanceof ASKError) {
+      return new Response(error.message, { status: 400 });
+    }
+
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  const { user } = session;
-
-  if (!user) {
-    return Response.redirect("/login");
-  }
-
-  if (request.body === null) {
-    return new Response("Request body is empty", { status: 400 });
-  }
-
-  const { downloadUrl, pathname, url } = await put(
-    `${user.email}/${filename}`,
-    request.body,
-    {
-      access: "public",
-    },
-  );
-
-  const content = await getPdfContentFromUrl(downloadUrl);
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-  });
-  const chunkedContent = await textSplitter.createDocuments([content]);
-
-  const { embeddings } = await embedMany({
-    model: registry.textEmbeddingModel("openai:text-embedding-3-small"),
-    values: chunkedContent.map((chunk) => chunk.pageContent),
-  });
-
-  await insertChunks({
-    chunks: chunkedContent.map((chunk, i) => ({
-      chunkRef: `${user.email}/${filename}/${i}`,
-      filePath: `${user.email}/${filename}`,
-      content: chunk.pageContent,
-      embedding: embeddings[i],
-      embeddingVector: embeddings[i],
-    })),
-  });
-
-  return Response.json({ pathname, url });
 }
