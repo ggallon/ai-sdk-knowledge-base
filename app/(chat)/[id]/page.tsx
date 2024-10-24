4c43ab124ea49a39eb5fe813d0c97861a4b4f83f
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-10-24 19:33:23 +0200

    Redirect unauthenticated user

--- app/(chat)/[id]/page.tsx
+++ app/(chat)/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { auth } from "@/app/(auth)/auth";
 import { Chat as PreviewChat } from "@/components/chat";
 import { getChatByPublicId } from "@/drizzle/query/chat";
@@ -10,16 +10,18 @@ interface PageProps {
 }
 
 export default async function Page(props: PageProps) {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   const { id } = await props.params;
   const chat = await getChatByPublicId({ publicId: id });
-
   if (!chat) {
     notFound();
   }
 
-  const session = await auth();
-
-  if (chat.author !== session?.user?.email) {
+  if (chat.author !== session.user?.email) {
     notFound();
   }
 
--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,9 +1,14 @@
 import { generateId } from "ai";
+import { redirect } from "next/navigation";
 import { auth } from "@/app/(auth)/auth";
 import { Chat } from "@/components/chat";
 
 export default async function Page() {
   const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return (
     <Chat publicId={generateId()} initialMessages={[]} session={session} />
   );
