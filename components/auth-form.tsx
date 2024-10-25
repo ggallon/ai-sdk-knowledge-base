80aaa540d66303beb4cb93ac554fdbf626e7021d
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-10-25 22:50:27 +0200

    Best enabling password AutoFill

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -60,7 +60,7 @@ export function AuthForm({
           name="email"
           type="email"
           placeholder="user@acme.com"
-          autoComplete="email"
+          autoComplete="username"
           required
           className="mt-1 block w-full appearance-none rounded-md bg-zinc-100 px-3 py-2 placeholder-zinc-400 focus:border-blue-500 focus:outline-none focus:ring-black sm:text-sm dark:bg-zinc-700 dark:text-zinc-300"
         />
@@ -76,6 +76,9 @@ export function AuthForm({
           id="password"
           name="password"
           type="password"
+          autoComplete={
+            type === "register" ? "new-password" : "current-password"
+          }
           required
           className="mt-1 block w-full appearance-none rounded-md bg-zinc-100 px-3 py-2 placeholder-zinc-400 focus:border-blue-500 focus:outline-none focus:ring-black sm:text-sm dark:bg-zinc-700 dark:text-zinc-300"
         />
