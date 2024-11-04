f2deae62acb48952152391db7adbfb6f23b4a26f
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-11-04 22:34:18 +0100

    Refactor drizzle (#40)
    * Remove DrizzleClient type
    
    * Rename drizzle tables
    
    * Use drizzle schema "public"
    
    * Revert "Use drizzle schema "public""
    
    This reverts commit b1395d84913dd85ee810328c2593e83642a781a0.

--- drizzle/query/user.ts
+++ drizzle/query/user.ts
@@ -1,32 +1,32 @@
 import { eq } from "drizzle-orm";
 import { db } from "@/drizzle/db";
-import { UserTable, type User } from "@/drizzle/schema";
+import { userTable, type User } from "@/drizzle/schema";
 import { hashPassword } from "@/utils/hash";
 
 export async function getUserByEmail(
   email: User["email"],
 ): Promise<Omit<User, "password"> | undefined> {
-  return await db.query.UserTable.findFirst({
+  return await db.query.userTable.findFirst({
     columns: { password: false },
-    where: eq(UserTable.email, email),
+    where: eq(userTable.email, email),
   });
 }
 
 export async function getUserById(
   userId: User["id"],
 ): Promise<Omit<User, "password"> | undefined> {
-  return await db.query.UserTable.findFirst({
+  return await db.query.userTable.findFirst({
     columns: { password: false },
-    where: eq(UserTable.id, userId),
+    where: eq(userTable.id, userId),
   });
 }
 
 export async function getUserWithPassword(
   email: User["email"],
-): Promise<Omit<User, "createdAt"> | undefined> {
-  return await db.query.UserTable.findFirst({
+): Promise<Pick<User, "id" | "email" | "password"> | undefined> {
+  return await db.query.userTable.findFirst({
     columns: { id: true, email: true, password: true },
-    where: eq(UserTable.email, email),
+    where: eq(userTable.email, email),
   });
 }
 
@@ -35,5 +35,5 @@ export async function createUser(
   password: User["password"],
 ) {
   const hash = await hashPassword(password);
-  return await db.insert(UserTable).values({ email, password: hash });
+  return await db.insert(userTable).values({ email, password: hash });
 }
